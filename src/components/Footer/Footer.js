625c4315175ebd60e1611fca5b54138efa1bcdaa
Ricky <mr.emps94@gmail.com>
2021-03-14 00:46:54 +0100

    fix style social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,6 +17,9 @@ export default function Footer() {
                         <a href="https://www.instagram.com/digitalshinobidiabolist/" target="_blank"><i class="fab fa-instagram"></i></a>
                         <a href="https://www.twitter.com/ActivistShinobi/" target="_blank"><i class="fab fa-twitter"></i></a>
                 </div>
+                <span className="dst_footer_copyright">
+                    The Shinobi Arts of Eccentricity non costituisce una testata giornalistica. Non ha carattere periodico ed è un sito aggiornato secondo le disponibilità e la reperibilità dei materiali. Pertanto non può essere considerato in alcun modo un prodotto editoriale ai sensi della Legge n.62 del 2001 e alla sentenza Corte cost. del 10 Marzo 2009 n. 10535. Non è soggetto a registrazione telematica in quanto non percepisce sovvenzioni dal fondo dell'editoria. I contenuti di "the-shinobi-arts-of-eccentricity.com" non possono essere messi sotto sequestro poiché l’art. 21, comma 3, Cost. riserva soltanto alla stampa, inoltre non possono essere copiati, riprodotti, pubblicati o redistribuiti in qualsiasi modo o forma se non come stabilito dall'Amministratore.
+                </span>
             </div>
         </footer>
     )
